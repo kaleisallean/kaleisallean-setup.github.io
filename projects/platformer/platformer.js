3506c9843612f703a4df2750fb4c0e411ccdf3da
kalei sallean <Kaleisallean6@gmail.com>
2023-09-18 17:01:50 +0000

    added platforms and collectables to game

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -61,9 +61,9 @@ createPlatform(1300,150,100,20)
     // You must decide on the collectable type, the x position, the y position, the gravity, and the bounce strength
     // Your collectable choices are 'database' 'diamond' 'grace' 'kennedi' 'max' and 'steve'; more can be added if you wish
     // example usage: createCollectable(type, x, y, gravity, bounce)
-createCollectable("database",600, 100);
-createCollectable("database",700, 500);
-createCollectable("database",1300, 100);
+createCollectable("bat",600, 100);
+createCollectable("bat",700, 500);
+createCollectable("bat",1300, 100);
     // TODO 3
     // Create cannons
     // You must decide the wall you want the cannon on, the position on the wall, and the time between shots in milliseconds
